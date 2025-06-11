935351bc4f3839741932fe40bdb9afa56f1ede51
tomabadie <abadie.thomas@gmail.com>
2025-06-11 16:40:12 +0200

    Done corrections after Biome check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,15 @@ function App() {
       </header>
 
       <main>
-        <h2 >Main</h2>
-        <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cumque esse, repellat tenetur mollitia illum neque accusantium minima ipsum voluptatum reiciendis reprehenderit at eligendi corporis. Officiis ex accusantium non praesentium odio ducimus, suscipit rerum nisi perferendis, mollitia, voluptate eaque consequatur assumenda veniam. Est, dignissimos ducimus incidunt accusamus officia pariatur fugit.</p>
+        <h2>Main</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cumque
+          esse, repellat tenetur mollitia illum neque accusantium minima ipsum
+          voluptatum reiciendis reprehenderit at eligendi corporis. Officiis ex
+          accusantium non praesentium odio ducimus, suscipit rerum nisi
+          perferendis, mollitia, voluptate eaque consequatur assumenda veniam.
+          Est, dignissimos ducimus incidunt accusamus officia pariatur fugit.
+        </p>
       </main>
 
       <footer>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
@@ -7,26 +6,21 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 import App from "./App";
 
-
 /* ************************************************************************* */
-
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-
 ]);
 
 /* ************************************************************************* */
-
 
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
-
 
 createRoot(rootElement).render(
   <StrictMode>
