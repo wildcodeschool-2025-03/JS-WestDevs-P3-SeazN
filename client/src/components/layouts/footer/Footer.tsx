43fbffe890d6e6cb330f0286a08389d2edd1ad7d
Leïla <leila-dahmani@live.fr>
2025-06-20 11:04:17 +0200

    Created footer + added github logo

--- client/src/components/layouts/footer/Footer.tsx
+++ client/src/components/layouts/footer/Footer.tsx
@@ -1,5 +1,21 @@
+import "./footer.css";
+
 const Footer = () => {
-  return <span>Footer</span>;
+  return (
+    <footer className="footer">
+      <div className="footer-right">
+        <a href="/contact">Nous contacter</a>
+        <a href="/mentions-legales">Mentions légales</a>
+        <a href="/cgu">CGU</a>
+
+        <img
+          src="/icones-logo/github-mark-white.svg"
+          alt="GitHub"
+          className="github-logo"
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
