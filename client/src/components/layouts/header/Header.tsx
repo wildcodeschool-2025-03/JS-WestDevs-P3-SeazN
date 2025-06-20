4b7495e3ed7fee2e40b2199c2fe5f8cc19584b0b
Leïla <leila-dahmani@live.fr>
2025-06-20 14:28:24 +0200

    made the link to language and sign in + created the begining of the left header with user name, avatar and research

--- client/src/components/layouts/header/Header.tsx
+++ client/src/components/layouts/header/Header.tsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router";
 import "./Header.css";
 
 const Header = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const userName = "Jean-Michmich";
+
   return (
     <header className="header">
+      <div className="header-left">
+        <img
+          src="/icones-logo/avatar-dg.jpg"
+          alt="Avatar de l'utilisateur"
+          className="user-avatar"
+        />
+
+        <span className="user-name">{userName}</span>
+        <button
+          type="button"
+          className="search-toggle"
+          onClick={() => setIsSearchVisible((prev) => !prev)}
+        >
+          🔍
+        </button>
+
+        {isSearchVisible && (
+          <input
+            type="search"
+            className="search-input"
+            placeholder="Rechercher une recette, ingrédient..."
+          />
+        )}
+      </div>
+
       <div className="header-right">
-        <button type="button">Langage</button>
-        <button type="button">Se connecter</button>
+        <Link to="/langage" className="header-link">
+          Langage
+        </Link>
+        <Link to="/connexion" className="header-link">
+          Se connecter
+        </Link>
       </div>
     </header>
   );
