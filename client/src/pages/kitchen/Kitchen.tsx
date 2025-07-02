4ea055f93abe82c2a00b5153a701ce7943283cb8
tomabadie <abadie.thomas@gmail.com>
2025-07-02 19:20:08 +0200

    Add Kitchen and Products pages to access subsections

---
+++ client/src/pages/kitchen/Kitchen.tsx
@@ -0,0 +1,16 @@
+const Kitchen = () => {
+  return (
+    <div>
+      <h2>Kitchen :</h2>
+      <ul>
+        <li><a href="/kitchen/myrecipes">My Recipes</a></li>
+        <li><a href="/kitchen/fridge">Fridge</a></li>
+        <li><a href="/kitchen/shoppinglist">Shopping List</a></li>
+        <li><a href="/kitchen/settings">Settings</a></li>
+        <li><a href="/kitchen/planner">Planner</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Kitchen;
---
+++ client/src/pages/products/Products.tsx
@@ -0,0 +1,17 @@
+const Products = () => {
+  return (
+    <div>
+      Products :
+      <ul>
+        <li>
+          <a href="/products/seasonnal">Seasonnal</a>
+        </li>
+        <li>
+          <a href="/products/analysis">Analysis</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Products;
