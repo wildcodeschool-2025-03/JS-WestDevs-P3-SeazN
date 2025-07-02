bf0136f50a81f2375c2ea47d928f454cfb7b60eb
tomabadie <abadie.thomas@gmail.com>
2025-07-02 19:56:22 +0200

    Create recipes search form - WIP

--- client/src/pages/recipes/Recipes.tsx
+++ client/src/pages/recipes/Recipes.tsx
@@ -1,5 +1,36 @@
+import "./Recipes.css"
+
 const Recipes = () => {
-  return <span>Recipes</span>;
+  return (
+    <section className="recipe-search">
+      <form action="">
+        Form :
+        <fieldset>
+          <legend>Recherche par nom</legend>
+          <label htmlFor="name">
+            <input id="name" type="text" name="name"
+              aria-label="name" placeholder="Recherche par nom" />
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Durée</legend>
+          <label htmlFor="">
+
+          </label>
+        </fieldset>
+
+        <label htmlFor="name">
+          <input id="name" type="text" name="name"
+            aria-label="name" placeholder="Recherche par nom" />
+        </label>
+
+      </form>
+      <div>
+        Visualisation :
+      </div>
+    </section>
+  );
 };
 
 export default Recipes;
