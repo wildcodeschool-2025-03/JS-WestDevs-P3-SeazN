31500c410e65c818f2421ce601afd0a8113b1a3c
tomabadie <abadie.thomas@gmail.com>
2025-07-10 18:51:07 +0200

    Set up SQL dynamic request (functionnal) for some filters - WIP

--- server/src/modules/recipes/recipesActions.ts
+++ server/src/modules/recipes/recipesActions.ts
@@ -1,19 +1,36 @@
 import type { RequestHandler } from "express";
-import recipesRepository, { type RecipesParams } from "./recipesRepository";
+import recipesRepository, {
+  type AvailableFilters,
+  type RecipesParams,
+} from "./recipesRepository";
 
 const browseSearchRecipes: RequestHandler = async (req, res, next) => {
   try {
-    const { name, price, duration, usersRanking, ecoRanking } = req.query;
-    const prices = typeof price === "string" ? price.split(",") : "";
-    const durations = typeof duration === "string" ? duration.split(",") : "";
-
-    const recipesParams: RecipesParams = {
+    /* const name = typeof req.query.name === "string" ? req.query.name : "";
+    const price = typeof req.query.price === "string" ? req.query.price : "";
+    const duration =
+      typeof req.query.duration === "string" ? req.query.duration : "";
+    const usersRanking =
+      typeof req.query.usersRanking === "string" ? req.query.usersRanking : "";
+    const ecoRanking =
+      typeof req.query.ecoRanking === "string" ? req.query.ecoRanking : ""; 
+      
+      const recipesParams: RecipesParams = {
       name,
-      prices,
-      durations,
+      price,
+      duration,
       usersRanking,
       ecoRanking,
     };
+    */
+
+    const filters = ["name", "price", "duration", "usersRanking", "ecoRanking"];
+    const recipesParams: RecipesParams = {};
+    for (const key of filters) {
+      const value = req.query[key];
+      recipesParams[key as AvailableFilters] =
+        typeof value === "string" ? value : "";
+    }
 
     console.warn("Filters : ", recipesParams);
 
--- server/src/modules/recipes/recipesRepository.ts
+++ server/src/modules/recipes/recipesRepository.ts
@@ -6,6 +6,13 @@ interface RecipeBase {
   image: string;
 }
 
+export type AvailableFilters =
+  | "name"
+  | "price"
+  | "duration"
+  | "usersRanking"
+  | "ecoRanking";
+
 export interface RecipesParams {
   name?: string;
   price?: string;
@@ -16,12 +23,62 @@ export interface RecipesParams {
 
 class RecipesRepository {
   async readSearchRecipes(recipesParams: RecipesParams) {
-    const [rows] = await databaseClient.query<Rows>(
-      "select * from recipe WHERE price = ?",
-      recipesParams.price,
-    );
+    let sql = "SELECT id,name,image from recipe";
+    /* AJOUTER is_validated ! */
+    const sqlWhere = [];
+    const sqlParams: (string | number)[] = [];
 
-    console.log("rows :", rows);
+    if (recipesParams.name && recipesParams.name.length > 0) {
+      sqlWhere.push("recipe.name LIKE ?");
+      sqlParams.push(`%${recipesParams.name}%`);
+    }
+
+    if (recipesParams.price && recipesParams.price.length > 0) {
+      const prices = recipesParams.price
+        .split(",")
+        .map((price) => Number.parseInt(price));
+
+      sqlWhere.push(
+        `recipe.price IN (${prices.map((price) => "?").join(", ")})`,
+      );
+
+      for (const price of prices) {
+        sqlParams.push(price);
+      }
+    }
+
+    /* Change TIME to INT in table */
+    /*  if (recipesParams.duration && recipesParams.duration.length > 0) {
+      sqlWhere.push("recipe.duration IN ?");
+      sqlParams.push(
+        recipesParams.duration
+          .split(",")
+          .map((value) => Number.parseInt(value)),
+      ); 
+    }  */
+
+    /* JOIN here
+    if (recipesParams.usersRanking && recipesParams.usersRanking.length > 0) {
+      sqlWhere.push("recipe.usersRanking >= ?");
+      sqlParams.push(Number.parseInt(recipesParams.usersRanking));
+    } */
+
+    if (recipesParams.ecoRanking && recipesParams.ecoRanking.length > 0) {
+      sqlWhere.push("recipe.eco_average >= ?");
+      sqlParams.push(Number.parseInt(recipesParams.ecoRanking));
+    }
+
+    if (sqlWhere.length > 0) {
+      sql += ` WHERE ${sqlWhere.join(" AND ")}`;
+    }
+
+    console.warn("SQL Where : ", sqlWhere);
+    console.warn("SQL request : ", sql);
+    console.warn("SQL values : ", sqlParams);
+
+    const [rows] = await databaseClient.query<Rows>(sql, sqlParams);
+
+    console.warn("rows :", rows);
 
     return rows as RecipeBase[];
   }
